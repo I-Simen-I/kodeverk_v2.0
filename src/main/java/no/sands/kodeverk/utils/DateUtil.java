2e89c85c94ac7c7fb63da136134f6324152f98ad
Simen Søhol <simen.so@outlook.com>
2015-04-13 21:44:44 +0200

    Endret timestampformat og lagt til støtte for at en dato kan være blank

--- src/main/java/no/sands/kodeverk/utils/DateUtil.java
+++ src/main/java/no/sands/kodeverk/utils/DateUtil.java
@@ -7,13 +7,13 @@ import java.text.SimpleDateFormat;
  * @author Simen Søhol
  */
 public class DateUtil {
-    private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm:ss.S";
+    private static final String TIMESTAMP_FORMAT = "yyyy-MM-dd HH:mm";
     private static final String DATE_FORMAT = "yyyy-MM-dd";
     private static final int DATE_LENGTH = 10;
-    private static final int TIMESTAMP_LENGTH = 21;
+    private static final int TIMESTAMP_LENGTH = 16;
 
     /**
-     * Checks if date format is yyyy-MM-dd hh:mm:ss.SSS
+     * Checks if date format is yyyy-MM-dd hh:mm
      *
      * @param date the date to validate
      * @return true if date is valid, otherwise false
@@ -29,7 +29,7 @@ public class DateUtil {
      * @return true if date is valid, otherwise false
      */
     public static boolean isDateValid(String date) {
-        return date.length() == DATE_LENGTH && validate(date, DATE_FORMAT);
+        return date.equals("") || date.length() == DATE_LENGTH && validate(date, DATE_FORMAT);
     }
 
     private static boolean validate(String date, String dateFormat) {
--- src/test/java/no/sands/kodeverk/utils/DateUtilTest.java
+++ src/test/java/no/sands/kodeverk/utils/DateUtilTest.java
@@ -12,24 +12,26 @@ public class DateUtilTest {
 
     @Test
     public void testValidateTimestamp() {
-        assertTrue(DateUtil.isTimestampValid("2015-01-10 10:00:00.0"));
-        assertTrue(DateUtil.isTimestampValid("2015-12-10 23:00:00.0"));
-        assertTrue(DateUtil.isTimestampValid("2015-01-10 10:59:59.0"));
-        assertTrue(DateUtil.isTimestampValid("2016-02-29 23:59:58.0"));
+        assertTrue(DateUtil.isTimestampValid("2015-01-10 10:00"));
+        assertTrue(DateUtil.isTimestampValid("2015-12-10 23:00"));
+        assertTrue(DateUtil.isTimestampValid("2015-01-10 10:59"));
+        assertTrue(DateUtil.isTimestampValid("2016-02-29 23:59"));
     }
 
     @Test
     public void testInvalidateTimestamp() {
+        assertFalse(DateUtil.isTimestampValid(""));
         assertFalse(DateUtil.isTimestampValid("2015-12-02"));
-        assertFalse(DateUtil.isTimestampValid("2015-02-29 10:00:00.0"));
-        assertFalse(DateUtil.isTimestampValid("2015-12-32 23:00:00.0"));
-        assertFalse(DateUtil.isTimestampValid("2015-01-10 10:60:59.0"));
+        assertFalse(DateUtil.isTimestampValid("2015-02-29 10:00"));
+        assertFalse(DateUtil.isTimestampValid("2015-12-32 23:00"));
+        assertFalse(DateUtil.isTimestampValid("2015-01-10 10:06:59.0"));
         assertFalse(DateUtil.isTimestampValid("2015-1d-10 10:60:59.0"));
-        assertFalse(DateUtil.isTimestampValid("2015-1-1 10:60:59.0"));
+        assertFalse(DateUtil.isTimestampValid("2015-1-1 10:60"));
     }
 
     @Test
     public void testValidateDate() {
+        assertTrue(DateUtil.isDateValid(""));
         assertTrue(DateUtil.isDateValid("2015-01-10"));
         assertTrue(DateUtil.isDateValid("2016-02-29"));
     }
