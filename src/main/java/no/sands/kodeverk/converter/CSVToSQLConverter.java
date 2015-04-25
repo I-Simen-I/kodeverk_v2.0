0e8cffc37a3e7c648c51dd95621271a5a2c46ccd
Simen Søhol <simen.so@outlook.com>
2015-04-25 09:31:26 +0200

    Lagt validator i en egen klasse

--- src/main/java/no/sands/kodeverk/converter/CSVToSQLConverter.java
+++ src/main/java/no/sands/kodeverk/converter/CSVToSQLConverter.java
@@ -17,50 +17,30 @@ import static no.sands.kodeverk.utils.SQLUtil.createInsertStatement;
 
 import java.io.File;
 import java.io.FileWriter;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
-
-import no.sands.kodeverk.helper.CSVErrorHelper;
 
 /**
  * @author Simen Søhol
  */
 public class CSVToSQLConverter {
-
-    private static final Logger LOGGER = Logger.getLogger(CSVToSQLConverter.class);
-
-    private CSVErrorHelper csvErrorHelper = new CSVErrorHelper();
-
     public Map<String, Integer> generateSQL() throws Exception {
         FileWriter fileWriter = new FileWriter(SQL_FILE_PATH + "inserts.sql");
-        Map<String, Integer> insertStats = new HashMap<String, Integer>();
-        List<String> errorList = new ArrayList<String>();
+        Map<String, Integer> insertStats = new HashMap<>();
 
         int insertCounter;
 
         for (File file : getFilesInFolder(KODEVERK_FILE_PATH)) {
             List<String[]> csvLines = readCSVFile(file);
             insertCounter = 0;
+            System.out.println(getFileName(file));
 
             for (int i = FIRS_KODEVERK_ROW_WITH_VALUES; i < csvLines.size(); i++) {
-                errorList.addAll(rowContainError(getFileName(file), csvLines, i));
-            }
-
-            if (errorList.isEmpty()) {
-                for (int i = FIRS_KODEVERK_ROW_WITH_VALUES; i < csvLines.size(); i++) {
-                    fileWriter.append(createInsertStatement(getFileName(file), getHeader(csvLines), getValuesToInsert(csvLines, i)));
-                    insertCounter++;
-                }
+                fileWriter.append(createInsertStatement(getFileName(file), getHeader(csvLines), getValuesToInsert(csvLines, i)));
+                insertCounter++;
             }
             insertStats.put(getFileName(file), insertCounter);
-        }
-        for (String error : errorList) {
-            LOGGER.log(Level.ERROR, error);
         }
 
         fileWriter.close();
@@ -97,12 +77,6 @@ public class CSVToSQLConverter {
         }
 
         return valueBuilder.toString();
-    }
-
-    private List<String> rowContainError(String fileName, List<String[]> csvList, int row) {
-        int numberOfValidColumns = getNumberOfValidInsertValues(csvList);
-
-        return csvErrorHelper.rowContainError(fileName, csvList, numberOfValidColumns, row);
     }
 
     private String addCommmaSeparator(int column, int columns) {
